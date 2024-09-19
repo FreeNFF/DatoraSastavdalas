ec2eb744a25170c10abc5760da9c31d21d394927
FreeNFF <nauisfricsons30@gmail.com>
2024-09-19 13:23:36 +0300

    s

--- datorsastavdalas.py
+++ datorsastavdalas.py
@@ -6,22 +6,43 @@ from PIL import Image, ImageTk
 
 #galvenā loga izveide
 logs = Tk()
-logs.geometry("350x400")
+logs.geometry("340x175")
 logs.title("Instrumentu noma")
 logs.configure(background="#b3ebf2")
 
+#bildes ievietošana
 bilde_frame = tk.Frame(logs)
-bilde_frame.grid(row=6, column=0)
+bilde_frame.grid(row=1, column=0, padx=5, pady=5)
 bilde_image =Image.open("images.png")
-resized_bilde = bilde_image.resize((150,125))
+resized_bilde = bilde_image.resize((150,125))#bildes garums
 bilde = ImageTk.PhotoImage(resized_bilde)
 bilde_label = ttk.Label(bilde_frame, image=bilde)
-bilde_label.grid(row=6, column=0)
+bilde_label.grid(row=1, column=0, padx=1, pady=1)
 
 #izveidot pogu
+btn=Button(logs, text="Pasūtījums", bd="5", command=lambda: pasutijums())#pievieno komandu, lai atvērtu citu logu
+btn1=Button(logs, text="Aizvērt", bd="5", command=logs.destroy)
+btn.grid(row=1, column=2, padx=10, pady=1)
+btn1.grid(row=1, column=3, padx=10, pady=1)
 
+#funkcija, kas izveido 2. līmeņa logu
+def pasutijums():
+    logs1 = Toplevel()
+    logs1.geometry("350x500")
+    logs1.title("Pasūtījums")
+    logs1.configure(background="#b3ebf2")
 
+    #teksti
+    ttk.Label(logs1, text="Izvēlaties darbinieku:", background="#b3ebf2").grid(row=1, column=0, padx=10, pady=10)
+    ttk.Label(logs1, text="Izvēlaties detaļu:", background="#b3ebf2").grid(row=2, column=0, padx=10, pady=10)
+    ttk.Label(logs1, text="Norādi cenu:", background="#b3ebf2").grid(row=3, column=0, padx=10, pady=10)
+    ttk.Label(logs1, text="Norādiet detaļas skaitu:", background="#b3ebf2").grid(row=4, column=0, padx=10, pady=10)
 
+    #mainīgie
+    vardsuzvards = tk.StringVar()
+    detala = tk.StringVar()
+    cena = tk.StringVar()
+    skaits = tk.StringVar()
 
 
 
